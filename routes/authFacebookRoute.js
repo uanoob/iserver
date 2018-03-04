e48843612f1e3a864f386cb0713c8586dd349443
uanoob <usnet@ukr.net>
2018-03-04 10:08:21 +0200

    Add mongodb, google auth

---
+++ routes/authFacebookRoute.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+  app.get(
+    '/auth/facebook',
+    passport.authenticate('facebook', {
+      scope: ['profile', 'email'],
+    }),
+  );
+
+  app.get('/auth/facebook/callback', passport.authenticate('facebook'), (req, res) => {
+    res.redirect('/home');
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+};
