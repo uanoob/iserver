aed425b5095d31873dc52e9e6e745a1dc6af812d
uanoob <usnet@ukr.net>
2018-03-05 10:09:33 +0200

    Add client

---
+++ client/src/containers/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import * as actions from '../redux/actions';
+
+import Header from '../components/Header';
+import Dashboard from '../components/Dashboard';
+import Landing from '../components/Landing';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div className="container">
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  fetchUser: PropTypes.func.isRequired,
+};
+
+export default connect(null, actions)(App);
---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import axios from 'axios';
+import App from './containers/App';
+import reducers from './redux/reducers/rootReducer';
+
+
+window.axios = axios;
+
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
